b28b655fa7380320be1c4bfc3abfcf9972121108
Matuesz <bieganski.m@wp.pl>
2020-03-13 18:32:53 +0100

    to be solved

--- lab3/stencil/stencil.cu
+++ lab3/stencil/stencil.cu
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define RADIUS        3000
+#define RADIUS        30
 #define NUM_ELEMENTS  1000000
 
 #define NUM_THREADS_PER_BLOCK  32 
-#define NUM_BLOCKS_PER_GRID    2
 
 static void handleError(cudaError_t err, const char *file, int line ) {
   if (err != cudaSuccess) {
@@ -17,48 +16,50 @@ static void handleError(cudaError_t err, const char *file, int line ) {
 #define cudaCheck( err ) (handleError(err, __FILE__, __LINE__ ))
 
 
-__device__ int in_range(int idx) {
+__device__ __host__ int in_range(int idx) {
   if (idx >= 0 && idx <= NUM_ELEMENTS -1)
     return 1;
   else
     return 0;
 }
 
-
-__global__ void stencil_1d(int *in, int *out) {
-  int i = threadIdx.x + (blockIdx.x * blockDim.x);
-  // int all = blockDim.x * gridDim.x;
-  // printf("me, all: %d, %d\n", idx, all);
-  
+__device__ __host__ void algo(int i, int *in, int *out) {
   for(int j = 0; j <= RADIUS; j++) {
     int idx1 = i - j;
-    if (!in_range(idx1))
-      continue;
-    out[i] += in[idx1];
+    if (in_range(idx1))
+      out[i] += in[idx1];
     if (j == 0)
       continue;
     int idx2 = i + j;
     if (!in_range(idx2))
-      continue;
-    out[i] += in[idx2];
+      out[i] += in[idx2];
   }
+}
+
+
+__global__ void stencil_1d(int *in, int *out) {
+  int i = threadIdx.x + (blockIdx.x * blockDim.x);
+  // int all = blockDim.x * gridDim.x;
+  if (i == 0)
+    printf("ZERO\n");
+  if(i == NUM_ELEMENTS - 1)
+    printf("MAX\n");
+  algo(i, in, out);
 }
 
 void cpu_stencil_1d(int *in, int *out) {
   for (int i = 0; i < NUM_ELEMENTS; i++) {
-    for(int j = 0; j <= RADIUS; j++) {
-      int idx1 = i - j;
-      if (idx1 < 0)
-        continue;
-      out[i] += in[idx1];
-      if (j == 0)
-        continue;
-      int idx2 = i + j;
-        if (idx1 > NUM_ELEMENTS - 1)
-          continue;
-      out[i] += in[idx2];
-    }
+    algo(i, in, out);
   }
+}
+
+int compare(int * r1, int * r2) {
+  int diffs = 0;
+  for (int i = 0; i < NUM_ELEMENTS; i++) {
+    if (r1[i] != r2[i])
+    diffs++;
+  }
+  return diffs;
 }
 
 int main() {
@@ -76,6 +77,8 @@ int main() {
   int *host_in = (int*)malloc(BYTES_NUM);
   int *host_out = (int*)malloc(BYTES_NUM);
 
+  int *from_dev_out = (int*)malloc(BYTES_NUM);
+
   //PUT YOUR CODE HERE - DEVICE MEMORY ALLOCATION
   cudaMalloc((void**)&dev_in, BYTES_NUM);
   cudaMalloc((void**)&dev_out, BYTES_NUM);
@@ -84,6 +87,7 @@ int main() {
   for(int i = 0; i < NUM_ELEMENTS; i++) {
     host_in[i] = i;
   }
+
   cudaMemcpy(dev_in, host_in, BYTES_NUM, cudaMemcpyHostToDevice);
   
   int num_blocks = NUM_ELEMENTS / NUM_THREADS_PER_BLOCK + 1;
@@ -98,7 +102,7 @@ int main() {
 
 
   //PUT YOUR CODE HERE - COPY RESULT FROM DEVICE TO HOST
-  cudaMemcpy(host_out, dev_out, BYTES_NUM, cudaMemcpyDeviceToHost);
+  cudaMemcpy(from_dev_out, dev_out, BYTES_NUM, cudaMemcpyDeviceToHost);
   
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
@@ -120,6 +124,13 @@ int main() {
   clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &cpu_stop);
   double result = (cpu_stop.tv_sec - cpu_start.tv_sec) * 1e3 + (cpu_stop.tv_nsec - cpu_start.tv_nsec) / 1e6;
   printf( "CPU execution time:  %3.1f ms\n", result);
+
+  int diffs = compare(from_dev_out, host_out);
+  if (diffs != 0)
+    printf("BAD: diff: %d/%d\n", diffs, NUM_ELEMENTS);
+  else
+    puts("OK, 0 diffs!\n");
+
   
   return 0;
 }
