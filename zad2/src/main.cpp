b4736d99bfd43aa3e87923e385f982bb4af96a52
Matuesz <bieganski.m@wp.pl>
2020-05-31 00:08:47 +0200

    V computation added

--- zad2/src/main.cpp
+++ zad2/src/main.cpp
@@ -12,6 +12,32 @@ char* FILE_PATH_OUT;
 int STEP_COUNT;
 double DELTA_TIME;
 
+const double E0 = 1.0;
+
+// https://stackoverflow.com/a/34660211
+float power(float base, unsigned int exp) {
+    if (exp == 0)
+       return 1;
+    float temp = power(base, exp / 2);       
+    if (exp % 2 == 0)
+        return temp*temp;
+    else {
+        if (exp > 0)
+            return base*temp*temp;
+        else
+            return (temp * temp) / base; // negative exponent computation 
+    }
+} 
+
+inline double compute_V(double rij, double rik, double rkj) {
+    double three = rij * rik * rkj;
+    double rij2 = rij * rij;
+    double rik2 = rik * rik;
+    double rkj2 = rkj * rkj;
+    return E0 * (1 / power(three, 3) + 3 * ((-rij2 + rik2 + rkj2) * (rij2 - rik2 + rkj2) * (rij2 + rik2 - rkj2)) 
+        / 8 * power(three, 5));
+}
+
 int parse_args (int argc, char **argv) {
     int gflag = 0, fflag = 0;
     int c;
