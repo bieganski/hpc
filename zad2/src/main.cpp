919228ec2732a171d762b36915e38dd44b47eb38
Matuesz <bieganski.m@wp.pl>
2020-06-07 00:51:30 +0200

    working on buffer split against number of nodes

--- zad2/src/main.cpp
+++ zad2/src/main.cpp
@@ -7,12 +7,20 @@
 #include <cstdint>
 #include <float.h>
 
+// #include <mpi.h>
+
 #include <iostream>
+
+#include "utils.hpp"
+
+
 bool VERBOSE;
 char* FILE_PATH_IN;
 char* FILE_PATH_OUT;
 int STEP_COUNT;
 double DELTA_TIME;
+uint32_t N;
+uint32_t NUM_PROC;
 
 const double E0 = 1.0;
 
@@ -42,46 +50,12 @@ double compute_V(double rij, double rik, double rkj) {
         / 8 * power(three, 5));
 }
 
-int parse_args (int argc, char **argv) {
-    int gflag = 0, fflag = 0;
-    int c;
-
-    opterr = 0;
-
-    if (argc != 5 && argc != 6) {
-        fprintf (stderr, "Usage: ./body3 particles_in.txt particles_out stepcount deltatime [-v]\n");
-        exit(1);
-    }
-    FILE_PATH_IN = argv[1];
-    FILE_PATH_OUT = argv[2];
-    try {
-        STEP_COUNT = std::stoi(std::string(argv[3]));
-    } catch  (... ) {
-        fprintf (stderr, "step_count must be proper unsigned int!\n");
-        exit(1);
-    }
-    try {
-        DELTA_TIME = std::stod(std::string(argv[4]));
-    } catch  (... ) {
-        fprintf (stderr, "delta_time must be proper double!\n");
-        exit(1);
-    }
-
-    if (argc == 6) {
-        if (std::strcmp(argv[5], "-v")) {
-            fprintf (stderr, "last argument expected is '-v'!\n");
-            exit(1);
-        }
-        VERBOSE = true;
-    } else {
-        VERBOSE = false;
-    }
-
-    return 0;
-}
-
-
 int main(int argc, char **argv) {
     parse_args(argc, argv);
+
+    // MPI_Init(NULL, NULL);
+    // MPI_Comm_size(MPI_COMM_WORLD, &NUM_PROC);
+
+    parse_input(get_input_content());
     return 0;
 }
