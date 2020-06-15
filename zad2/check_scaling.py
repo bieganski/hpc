ad9683005a22464968183caeab39e41e5bbb2043
Matuesz <bieganski.m@wp.pl>
2020-06-15 14:12:49 +0200

    scaling checking added

--- zad2/check_scaling.py
+++ zad2/check_scaling.py
@@ -1,9 +1,59 @@
 #!/usr/bin/env python3
 
 
+import time
+import subprocess, os
+from os.path import join
 
-command = "mpirun -n %d"
 
+
+command = "mpirun -n %d ./body3 %s %s %d %f"
+
+OUT_TMP = "tmp_out.remove"
+TEST_DIR = "tests"
+
+TEST_IN = "test_100.txt"
+
+# n, step, delta
+TESTS_WEAK = [
+    (4,  8,   0.5),
+    (8,  16,  0.5),
+    (16, 32,  0.5),
+    (32, 64,  0.5),
+    (64, 128, 0.5)
+]
+
+TESTS_STRONG = [
+    (4,  8, 0.5),
+    (8,  8, 0.5),
+    (16, 8, 0.5),
+    (32, 8, 0.5),
+    (64, 8, 0.5)
+]
+
+TIME_STRONG_SEQ = 191 # 3:11
+
+TIMES_WEAK_SEQ = [
+    
+]
+
+TIMES_WEAK = []
+TIMES_STRONG = []
+
+cmd = lambda n, s, d : command % (n, join(TEST_DIR, TEST_IN), OUT_TMP, s, d)
+
+
+def run(n,s,d):
+    start = time.time()
+    os.system(cmd(n,s,d))
+    print(time.time() - start)
+    exit(1)
+
+
+def all():
+    for n,s,d in TESTS_STRONG:
+        run(n,s,d)
+        break
 
 if __name__ == "__main__":
-    print(command % 1)
+    all()
