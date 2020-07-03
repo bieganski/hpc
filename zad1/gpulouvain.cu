9e994fa3d46c468285514329c9fbdeae58b265c3
Matuesz <bieganski.m@wp.pl>
2020-07-03 11:46:46 +0200

    test

--- zad1/gpulouvain.cu
+++ zad1/gpulouvain.cu
@@ -380,8 +380,8 @@ void contract(const uint32_t V_MAX_IDX,
 
     computeWTF(V, compressedComm, WTF);
 
-    print_DEBUG(V_MAX_IDX, RAW(WTF), "WTF");
-    print_DEBUG(V_MAX_IDX, RAW(edgePos), "edgePos");
+    print_DEBUG(V_MAX_IDX, RAW(WTF), "WTF", true);
+    print_DEBUG(V_MAX_IDX, RAW(edgePos), "edgePos", true);
 
     uint32_t E_size = V[V_MAX_IDX + 1];
     // Each call to `compute_comm _neighbors` kernel updates part of these values
