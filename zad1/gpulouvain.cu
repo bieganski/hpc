83aac2481c829827558204186b02891c8ef257a1
Matuesz <bieganski.m@wp.pl>
2020-07-03 11:12:48 +0200

    test

--- zad1/gpulouvain.cu
+++ zad1/gpulouvain.cu
@@ -341,8 +341,8 @@ void contract(const uint32_t V_MAX_IDX,
     // compressedCom <=> `com` from paper
     cudaDeviceSynchronize();
 
-    printf("[C]: size[1]: %ul, deg[1]: %ul\n", commSize[1], commDegree[1]);
-    printf("[C]: size[5]: %ul, deg[5]: %ul\n", commSize[5], commDegree[5]);
+    printf("[C]: size[1]: %lu, deg[1]: %lu\n", commSize[1], commDegree[1]);
+    printf("[C]: size[5]: %lu, deg[5]: %lu\n", commSize[5], commDegree[5]);
     printf("[C]: size[MAX]: %d, deg[MAX]: %d\n", commSize[V_MAX_IDX], commDegree[V_MAX_IDX]);
 
     thrust::exclusive_scan(commDegree.begin(), commDegree.end(), edgePos.begin());
