3ff18a643d5950dd6dd6dbf6cdcc741cf1f50a6b
Matuesz <bieganski.m@wp.pl>
2020-07-03 16:58:47 +0200

    test

--- zad1/hasharray.cu
+++ zad1/hasharray.cu
@@ -63,7 +63,10 @@ CUDA_CALLABLE_MEMBER bool HA::insertWithFeedback(KeyValueInt* h1, KeyValueFloat*
             return true;
         } else if (prev == key) {
             atomicAdd(&h2[slot].value, v2);
-            assert(h1[slot].value == v1);
+            if (h1[slot].value != v1) {
+                printf("UWAGA: chcę %d a był %d\n", v1, h1[slot].value);
+            }
+            // assert(h1[slot].value == v1);
             return false;
         }
         slot = (slot + 1) & (table_size - 1);
